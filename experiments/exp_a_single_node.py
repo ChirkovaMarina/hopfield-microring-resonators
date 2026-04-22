371b2805f292b76595ba2fdb18d17a3d8a3ef2be
ChirkovaMarina <69245459+ChirkovaMarina@users.noreply.github.com>
2026-04-22 14:05:16 +0900

    Update exp_a_single_node.py

--- experiments/exp_a_single_node.py
+++ experiments/exp_a_single_node.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 if __package__ is None or __package__ == "":
     sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 
-from src.physics import MRRParams, MicroringResonator
+from hopfield_nn.physics import MRRParams, MicroringResonator
 
 
 class ExpA_SingleNode:
